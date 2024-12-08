cbe44df338ed1c58ac24d8d493b11f7904ef5815
Eirik Steira <eirik.steira@alv.no>
2024-12-08 17:56:56 +0100

    Task 2 - solution

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,10 @@
+import 'package:alv_pong/match_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  // TODO 2.1: Legg til felter av en passende datatype til å holde styr på text-inputs for navn.
+  // Lagt til to felter: en kontroller for hver spiller
+  final TextEditingController player1Controller = TextEditingController();
+  final TextEditingController player2Controller = TextEditingController();
 
   HomeScreen({super.key});
 
@@ -15,18 +18,27 @@ class HomeScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextField(
-                  // TODO 2.1: send med feltet du lagde for første spiller
+                  // vi sender med kontrolleren i tekst-feltet
+                  controller: player1Controller,
                   decoration: const InputDecoration(labelText: "Spiller 1"),
                 ),
                 const SizedBox(height: 5),
                 TextField(
-                  // TODO 2.1: send med feltet du lagde for andre spiller
+                  // vi sender med kontrolleren i tekst-feltet
+                  controller: player2Controller,
                   decoration: const InputDecoration(labelText: "Spiller 2"),
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                     onPressed: () {
-                      // TODO 2.2: Gå til MatchScreen og send med spillernavn.
+                      // Dytt den nye ruta/skjermen på navigasjonen.
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MatchScreen(
+                                  // navnet på disse argumentene må passe feltene i MatchScreen
+                                  player1: player1Controller.text,
+                                  player2: player2Controller.text)));
                     },
                     child: const Text("Start matchen!"))
               ],
